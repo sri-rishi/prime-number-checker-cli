0cc57e8e527562deb9321efeb6cf6ed11cba95ea
Rishi Srivastava <72429483+sri-rishi@users.noreply.github.com>
2020-12-27 00:13:31 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var readlineSync = require("readline-sync");
+var chalk = require("chalk");
+
+var userName = readlineSync.question(chalk.red("Please! Enter your name- "));
+
+console.log("Welcome " + userName);
+console.log(chalk.cyan("Is your birthdate is a prime Number?"));
+var score = 0;
+function primeNumber(question) {
+  var userNumber = readlineSync.question(question);
+  
+  if(isNaN(userNumber)) {
+    console.log(chalk.bgRed("Error you have entered wrong type"));
+  }else{
+    for (var i = 2;i<userNumber;i++) {
+      if(userNumber % i === 0) {
+        score = score + 1;
+      }
+    }
+    if(score  === 0) {
+      console.log(chalk.yellowBright("Your birthdate is a prime number"));
+    } else {
+      console.log(chalk.blue("Sorry! Not a prime number"));
+    }
+  }
+}
+
+primeNumber(chalk.magenta("Enter your birthdate- "));
